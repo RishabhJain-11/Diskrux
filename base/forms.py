42b3af7649cf6682310e7c4d42e74ea72f3abbd9
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-06-27 17:42:29 +0530

    27-06-2023

--- base/forms.py
+++ base/forms.py
@@ -1,22 +1,15 @@
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, User
-
-
-class MyUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['name', 'username', 'email', 'password1', 'password2']
-
+from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
-    class Meta:
+    class Meta: 
         model = Room
-        fields = '__all__'
-        exclude = ['host', 'participants']
-
-
-class UserForm(ModelForm):
+        fields = "__all__"
+        exclude = ["host", "participants"]
+    
+class UserForm(ModelForm) : 
     class Meta:
-        model = User
-        fields = ['avatar', 'name', 'username', 'email', 'bio']
+        models = User
+        fields = ["username", "email"]
+        
