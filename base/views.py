0bebf6f8531cecc5e2246604573b412243310ee5
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-06-28 20:46:23 +0530

    28-06-2023

--- base/views.py
+++ base/views.py
@@ -3,11 +3,19 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import views as auth_views
 from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
+
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
+
 
 def loginPage(request):
     page = 'login'
